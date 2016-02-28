49dc39c114c05348951059b321b8cb353c812c12
d0zingcat <unclecattony@gmail.com>
2016-02-28 20:56:27 +0800

    Functions on 28th, Feb, 2016

--- main.py
+++ main.py
@@ -1,52 +1,64 @@
 # coding:utf-8
-import urllib
+import urllib, os
 
-domain = 'http://www.liaoxuefeng.com'           #廖雪峰的域名
-path = r'C:\Users\cyhhao2013\Desktop\temp\\'    #html要保存的路径
 
-# 一个html的头文件
-input = open(r'C:\Users\cyhhao2013\Desktop\0.html', 'r')
-head = input.read()
+# The amount of records in total
+totalRecord = 111
 
-# 打开python教程主界面
+# The main domain
+domain = 'http://www.liaoxuefeng.com'          
+
+# The storage path
+path = os.getcwd() + "\\" + r'html\\'    
+
+# The "Meta" data of a HTML file keeping the contents of '<head>' and '<body>' such information 
+data_input = open(r'meta.html', 'r')
+head = data_input.read()
+
+# To get the index URLs
 f = urllib.urlopen("http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000")
 home = f.read()
 f.close()
 
-# 替换所有空格回车（这样容易好获取url）
+# remove all the spaces and enters to get URL more easily
 geturl = home.replace("\n", "")
 geturl = geturl.replace(" ", "")
 
-# 得到包含url的字符串
-list = geturl.split(r'em;"><ahref="')[1:]
+# The URL links with other characters
+list = geturl.split(r'em;"><ahref="')[1:totalRecord]
 
-# 强迫症犯了，一定要把第一个页面也加进去才完美
+# The first Page
 list.insert(0, '/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000">')
 
-# 开始遍历url List
 for li in list:
     url = li.split(r'">')[0]
-    url = domain + url              #拼凑url
-    print url
+
+    # Merge the URL
+    url = domain + url              
     f = urllib.urlopen(url)
     html = f.read()
 
-    # 获得title为了写文件名
+    # To name the files
     title = html.split("<title>")[1]
     title = title.split(" - 廖雪峰的官方网站</title>")[0]
 
-    # 要转一下码，不然加到路径里就悲剧了
+    # To decode 
     title = title.decode('utf-8').replace("/", " ")
-
-    # 截取正文
-    html = html.split(r'<!-- block main -->')[1]
-    html = html.split(r'<h4>您的支持是作者写作最大的动力！</h4>')[0]
+    
+#     # To get the body content
+    html = html.split('<div class="x-content" style="width:100%">')[1].split(r'<div id="x-wiki-prev-next')[0]
+    
+    
     html = html.replace(r'src="', 'src="' + domain)
+    print "The %d(th) record has be taken down." % (list.index(li) + 1)
 
-    # 加上头和尾组成完整的html
-    html = head + html+"</body></html>"
+#     # Make Up complete html files using the meta template
+    html = head + "<title>%s</title>" % title.encode('utf-8') + html+"</body></html>"
 
-    # 输出文件
-    output = open(path + "%d" % list.index(li) + title + '.html', 'w')
+#     # Save files to local drive
+    output = open(path + "%d_" % (list.index(li) + 1) + title + '.html', 'w')
     output.write(html)
     output.close()
+
+# Print statistics in total
+print "Here are %d records in total" % len(list)
